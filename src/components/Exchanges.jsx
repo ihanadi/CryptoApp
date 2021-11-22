24b269ee5cd10770b0e572499f6fb43197b2c0d8
ihanadi <ismailhanadi41@gmail.com>
2021-11-23 00:42:54 +0300

    fix css bug

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -27,24 +27,25 @@ function Exchanges() {
         {exchangesList.map((exchange) => (
           <Col span={24}>
             <Collapse>
-            <Panel
-              key={exchange.id}
-              showArrow={false}
-              header={(
-                <Row key={exchange.id}>
-                  <Col span={6}>
-                    <Text><strong>{exchange.rank}.</strong></Text>
-                    <Avatar className="exchange-image" src={exchange.iconUrl}/>
-                    <Text><strong>{exchange.name}</strong></Text>
-                  </Col>
-                  <Col span={6}>${millify(exchange.volume)}</Col>
-                  <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                  <Col span={6}>{millify(exchange.marketShare)}%</Col>
-                </Row>
-              )}
-            >
-              {HTMLReactParser(exchange.description || '')}
-            </Panel>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                style={{display:"block"}}
+                header={(
+                  <Row key={exchange.id}>
+                    <Col span={6}>
+                      <Text><strong>{exchange.rank}.</strong></Text>
+                      <Avatar className="exchange-image" src={exchange.iconUrl}/>
+                      <Text><strong>{exchange.name}</strong></Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                  </Row>
+                )}
+              >
+                {HTMLReactParser(exchange.description || '')}
+              </Panel>
             </Collapse>
           </Col>
         ))}
